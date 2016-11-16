987761e008283533db3bbd0c3c8dd6cc711e4b90
Alex Burka <aburka@seas.upenn.edu>
2016-11-16 02:13:15 -0500

    show multiple phone numbers

--- website/call/models.py
+++ website/call/models.py
@@ -6,7 +6,7 @@ class Politician(models.Model):
     DENOUNCES = 'D'
     HOUSE = 'H'
     SENATE = 'S'
-    zipcode = models.CharField(max_length=5)
+    zip_or_state = models.CharField(max_length=5)
     district_or_class = models.PositiveSmallIntegerField()
     chamber = models.CharField(max_length=1,
                                choices=[(HOUSE, 'House of Representatives'),
@@ -17,15 +17,15 @@ class Politician(models.Model):
                                          (DENOUNCES, 'Denounces')],
                                 default=HAS_NOT_SAID)
     shown_to_all = models.BooleanField(default=False)
-    extra_phones = models.ForeignKey('Phone', null=True, blank=True)
     script = models.TextField(default=None, null=True, blank=True)
 
     def __str__(self):
-        return '%s-%s %s' % (self.zipcode, self.district_or_class, self.chamber)
+        return '%s-%s %s' % (self.zip_or_state, self.district_or_class, self.chamber)
 
 class Phone(models.Model):
     number = models.CharField(max_length=10)
     desc = models.TextField()
+    politician = models.ForeignKey('Politician')
 
     def __str__(self):
         return '%s: (%s) %s-%s' % (self.desc, self.number[0:3], self.number[3:6], self.number[6:])
--- website/call/views.py
+++ website/call/views.py
@@ -7,25 +7,40 @@ def index(request):
     return render(request, 'call/index.html', context)
 
 def add_local_house(zipcode, critter):
-    matching = Politician.objects.filter(chamber=Politician.HOUSE, zipcode=zipcode, district_or_class=critter.district)
+    matching = Politician.objects.filter(chamber=Politician.HOUSE, zip_or_state=zipcode, district_or_class=critter.district)
     if len(matching) == 1:
-        return critter._replace(local_phones = matching[0].extra_phones,
+        return critter._replace(local_phones = matching[0].phone_set.all(),
+                                custom_script = matching[0].script)
+    else:
+        return critter
+
+def add_local_senate(state, critter):
+    matching = Politician.objects.filter(chamber=Politician.SENATE, zip_or_state=state, district_or_class=critter.class_)
+    print('checking %s against %s' % (critter, matching))
+    if len(matching) == 1:
+        return critter._replace(local_phones = matching[0].phone_set.all(),
                                 custom_script = matching[0].script)
     else:
         return critter
 
 def scripts(request):
     zipcode = request.GET['zip']
+    (city, state) = scrape.zip_code_city_state(zipcode)
     reps = list(map(lambda r: add_local_house(zipcode, r), scrape.get_representatives(zipcode)))
+    sens = list(map(lambda s: add_local_senate(state, s), scrape.get_senators(state)))
     global_reps = Politician.objects.filter(shown_to_all=True, chamber=Politician.HOUSE)
-    global_sens = []
+    global_sens = Politician.objects.filter(shown_to_all=True, chamber=Politician.SENATE)
     for critter in global_reps:
         critter.rep = filter(lambda r: r.district == critter.district_or_class,
-                             scrape.get_representatives(critter.zipcode)).__next__()
+                             scrape.get_representatives(critter.zip_or_state)).__next__()
+    for critter in global_sens:
+        critter.rep = filter(lambda r: r.class_ == critter.district_or_class,
+                             scrape.get_senators(critter.zip_or_state)).__next__()
+        print(critter.rep)
     context = {'name': request.GET.get('name', '$NAME'),
-               'city': '$CITY',
-               'state': '$STATE',
-               'senators': [],
+               'city': city,
+               'state': state,
+               'senators': sens,
                'representatives': reps,
                'global_representatives': global_reps,
                'global_senators': global_sens}
