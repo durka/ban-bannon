6b4b980fd6a93301472c847447d10d7fa34f8972
Alex Burka <aburka@seas.upenn.edu>
2016-11-15 23:29:53 -0500

    move scrape.py

--- website/call/views.py
+++ website/call/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from call import scrape
 
 def index(request):
     context = {}
     return render(request, 'call/index.html', context)
 
 def scripts(request):
-    context = {}
+    context = {'zip': request.POST['zip']}
     return render(request, 'call/scripts.html', context)
 
